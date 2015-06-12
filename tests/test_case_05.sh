7de6a4ba48c6f4fb816d5566aacd0ca96034faa7
Nicolas Carrier <nicolas.carrier@parrot.com>
2015-06-12 13:39:20 +0200

    [DEVMINOR] fixed test case indentation
    Change-Id: Ia11dde8d2f15eee49e44c0de40220203137a5181

--- tests/test_case_05.sh
+++ tests/test_case_05.sh
@@ -7,12 +7,12 @@
 turns=${turns:-4}
 
 for (( i=1; i < $turns; i++)); do
-		(pomp-cli --timeout 10 --dump --wait i unix:/var/run/firmwared.sock i %s PING) &
-		pid[i]=$!
-		echo "launched command "${pid[i]}
+	(pomp-cli --timeout 10 --dump --wait i unix:/var/run/firmwared.sock i %s PING) &
+	pid[i]=$!
+	echo "launched command "${pid[i]}
 done
 
 for (( i=1; i < $turns; i++)); do
-		echo "wait for command "${pid[i]}
-				wait ${pid[i]}
+	echo "wait for command "${pid[i]}
+	wait ${pid[i]}
 done
