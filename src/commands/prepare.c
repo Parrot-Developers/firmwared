c13b311ab5becf9b48102d1a81ea10a65f227376
Nicolas Carrier <nicolas.carrier@parrot.com>
2015-06-23 15:53:55 +0200

    [DEV] implementation of the new prepare command
    Change-Id: Id15ef38d4cde28f5adfd9b5031a98c60e7ddfe29

--- src/commands/prepare.c
+++ src/commands/prepare.c
@@ -37,50 +37,41 @@ static int prepare_command_handler(struct pomp_conn *conn,
 {
 	int ret;
 	char __attribute__((cleanup(ut_string_free)))*cmd = NULL;
-	char __attribute__((cleanup(ut_string_free))) *identifier = NULL;
-	struct instance *instance;
+	char __attribute__((cleanup(ut_string_free))) *folder = NULL;
+	char __attribute__((cleanup(ut_string_free))) *identification_string = NULL;
 
-	ret = pomp_msg_read(msg, "%ms%ms", &cmd, &identifier);
+	ret = pomp_msg_read(msg, "%ms%ms%ms", &cmd, &folder,
+			&identification_string);
 	if (ret < 0) {
-		cmd = identifier = NULL;
+		cmd = folder = identification_string = NULL;
 		ULOGE("pomp_msg_read: %s", strerror(-ret));
 		return ret;
 	}
 
-	instance = instance_new(identifier);
-	if (instance == NULL) {
-		ret = -errno;
-		ULOGE("instance_new: %m");
-		return ret;
-	}
-	/* folder_store transfers the ownership of the entity to the folder */
-	ret = folder_store(INSTANCES_FOLDER_NAME, instance_to_entity(instance));
-	if (ret < 0) {
-		instance_delete(&instance, false);
-		ULOGE("folder_store: %s", strerror(-ret));
-		return ret;
-	}
-
-	return firmwared_notify(pomp_msg_get_id(msg), "%s%s%s%s%s",
-			"PREPARED", "placeholder",
-			"placeholder",
-			instance_get_sha1(instance),
-			instance_get_name(instance));
+	return folder_prepare(folder, identification_string,
+			pomp_msg_get_id(msg));
 }
 
 static const struct command prepare_command = {
 		.name = COMMAND_NAME,
-		.help = "Creates an instance of the given firmware, in the "
-				"READY state.",
-		.long_help = "If FIRMWARE_IDENTIFIER doesn't correspond to a "
-				"registered firmware, then it is supposed to "
-				"be a path to a directory which will be "
-				"mounted as the read-only layer for the "
-				"prepared instance, this allows to create "
-				"instances from the final directory of a "
-				"firmware's workspace and is intended for "
-				"development.",
-		.synopsis = "FIRMWARE_IDENTIFIER",
+		.help = "Creates an instance from a firmware, in the READY "
+				"state, of create a firmware from an URL, a "
+				"path to a final directory or a path to an "
+				"ext2 image of a firmware.",
+		.long_help = "If FOLDER equals to firmware, then "
+				"IDENTIFICATION_STRING can be a path or an url "
+				"in this case, the corresponding firmware will "
+				"be retrieved using curl. "
+				"It can also be a path to a final folder, a "
+				"firmware will then be registered from this "
+				"directory.\n"
+				"If FOLDER equals to instance, then "
+				"IDENTIFICATION_STRING must be either a sha1 "
+				"or a friendly name of a previously registered "
+				"firmware. "
+				"A new instance will then be created and "
+				"registered from this firmware.",
+		.synopsis = "FOLDER IDENTIFICATION_STRING",
 		.handler = prepare_command_handler,
 };
 
