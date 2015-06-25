d465fb29b966e3441f560c0dde79d8652fb0d61d
Nicolas Carrier <nicolas.carrier@parrot.com>
2015-06-25 11:47:52 +0200

    [DEV] adapted launch_instance.sh to the new prepare command
    Change-Id: I507ed98f26ed032cce6cfda55c023a6c5c06a61b

--- examples/launch_instance.sh
+++ examples/launch_instance.sh
@@ -9,15 +9,48 @@ then
 	exit
 fi
 
-set -x
+if [ -n "${V+x}" ]
+then
+	set -x
+fi
+
+set -e
 
 . out/sphinx-base/staging/native-wrapper.sh
 
 # defaults to the first firmware found
 firmware=${firmware:-$(fdc list firmwares | sed 's/\[.*//g')}
 
+if [ -z "${firmware}" ]; then
+	echo "No suitable firmware found, please prepare at least one or set"
+	echo "the firmware environment variable to the final directory you "
+	echo "want to use as a firmware."
+	exit 1
+fi
+
+drop_firmware="false"
+# check if the firmware is a registered one, otherwise, consider it as an
+# argument for an 'fdc prepare firmware' command
+if [ -z "$(fdc list firmwares | grep ${firmware})" ]; then
+	drop_firmware="true"
+	firmware=$(fdc prepare firmwares ${firmware})
+	if [ -n "$(echo ${firmware} | grep "File exists")" ]; then
+		echo "Firmware already registered."
+		echo -n "If it's normal, just retry with the "
+		echo "\"firmware\" environment variable set to an id of this "
+		echo "firmware."
+		exit 1
+	fi
+	firmware=$(echo ${firmware} | tail -n 1 | sed 's/.*: //g')
+fi
+
+if [ -z "${firmware}" ]; then
+	echo "something went wrong, no firmware defined, aborting"
+	exit 1
+fi
+
 # create an instance and store it's name for future use
-instance=$(fdc prepare ${firmware} | tail -n 1 | sed 's/.*: //g');
+instance=$(fdc prepare instances ${firmware} | tail -n 1 | sed 's/.*: //g');
 
 fdc show instances $instance
 # set which process will be used as the pid 1
@@ -52,3 +85,6 @@ read -p "Press enter to end the session ..."
 # cleanup
 fdc kill $instance
 fdc drop instances $instance
+if [ "${drop_firmware}" = "true" ]; then
+	fdc drop firmwares ${firmware}
+fi
