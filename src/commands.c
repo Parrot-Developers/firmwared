8c9565827cf53feca902ff0bdbe77da098b29040
Nicolas Carrier <nicolas.carrier@parrot.com>
2015-08-21 09:28:24 +0200

    [COVERITY 80630] if no command is registered, list can be NULL
    Change-Id: I8b77efc7a576889b23a676c52c18858619a68063

--- src/commands.c
+++ src/commands.c
@@ -197,6 +197,11 @@ const char *command_list(void)
 			return NULL;
 		}
 	}
+	if (list == NULL) {
+		ULOGE("no command registered, that _is_ weird");
+		errno = ENOENT;
+		return NULL;
+	}
 	if (list[0] != '\0')
 		list[strlen(list) - 1] = '\0';
 
