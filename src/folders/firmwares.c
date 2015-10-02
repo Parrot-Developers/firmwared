7f416f25e17dc7833f7bb22ae16984872ecd4a92
Nicolas Carrier <nicolas.carrier@parrot.com>
2015-10-02 16:06:00 +0200

    [DEV] redirect error to ulogw when preparing firmwares
    Change-Id: I1a1cfe62d02f49216c6f0956315fba403109251c

--- src/folders/firmwares.c
+++ src/folders/firmwares.c
@@ -402,7 +402,7 @@ static int firmware_preparation_start(struct preparation *preparation)
 				.stdout_sep_cb = preparation_progress_sep_cb,
 				.out_sep1 = '\n',
 				.out_sep2 = IO_SRC_SEP_NO_SEP2,
-				.stderr_sep_cb = log_dbg_src_sep_cb,
+				.stderr_sep_cb = log_warn_src_sep_cb,
 				.err_sep1 = '\n',
 				.err_sep2 = IO_SRC_SEP_NO_SEP2,
 				.timeout = PREPARATION_TIMEOUT,
