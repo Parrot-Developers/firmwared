f6dee1c2aab97dc92b1398b17b739131c71c201c
Nicolas Carrier <nicolas.carrier@parrot.com>
2015-12-08 09:30:44 +0100

    [DEV] removed obsolete tests
    Change-Id: If73bcf60d8cd70d6ae9b6c770dae17e20b45a898

--- tests/test_case_03.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -x
-
-instance=$(fdc prepare /opt2/work/dragonfly/out/dragonfly-mykonos3_sim_pc/final | tail -n 1 | sed 's/.*: //g');
-fdc drop instances $instance
-instance=$(fdc prepare /opt2/work/dragonfly/out/dragonfly-mykonos3_sim_pc/final | tail -n 1 | sed 's/.*: //g');
-instance=$(fdc prepare /opt2/work/dragonfly/out/dragonfly-mykonos3_sim_pc/final | tail -n 1 | sed 's/.*: //g');
