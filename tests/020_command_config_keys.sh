49b50abee56c0f0c0255b4bf62847ee3ba3179fd
Nicolas Carrier <nicolas.carrier@parrot.com>
2016-01-14 10:32:21 +0100

    [DEV] config keys list has changed, adapt the test
    Change-Id: Ie59f7028efe04fbc5c79844255b7ad37a0252007

--- tests/020_command_config_keys.sh
+++ tests/020_command_config_keys.sh
@@ -10,5 +10,5 @@ fi
 set -eu
 
 answer=$(fdc config_keys)
-expected="apparmor_profile container_interface curl_hook disable_apparmor dump_profile host_interface_prefix mount_hook mount_path net_first_two_bytes net_hook prevent_removal resources_dir repository_path socket_path verbose_hook_scripts"
+expected="apparmor_profile apply_perms_hook container_interface curl_hook disable_apparmor dump_profile host_interface_prefix mount_hook mount_path net_first_two_bytes net_hook prevent_removal resources_dir repository_path socket_path verbose_hook_scripts"
 [ "${answer}" = "${expected}" ]
