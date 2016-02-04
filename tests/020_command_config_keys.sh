2372a14cbde7080ff96314bf878f98314701006b
Nicolas Carrier <nicolas.carrier@parrot.com>
2016-02-04 08:54:04 +0100

    [DEV] repair config_keys automatic test
    Change-Id: I38f48d8d8abaafc616611c1c17b2b777b4836128

--- tests/020_command_config_keys.sh
+++ tests/020_command_config_keys.sh
@@ -10,5 +10,5 @@ fi
 set -eu
 
 answer=$(fdc config_keys)
-expected="apparmor_profile container_interface curl_hook disable_apparmor dump_profile host_interface_prefix mount_hook mount_path net_first_two_bytes net_hook post_prepare_instance_hook prevent_removal resources_dir repository_path socket_path verbose_hook_scripts"
+expected="apparmor_profile container_interface curl_hook disable_apparmor dump_profile host_interface_prefix mount_hook mount_path net_first_two_bytes net_hook post_prepare_instance_hook prevent_removal resources_dir repository_path socket_path x11_path nvidia_path verbose_hook_scripts"
 [ "${answer}" = "${expected}" ]
